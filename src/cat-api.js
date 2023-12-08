9f9c4a15dd17499edb5b8b055538be54e4f7b03f
inezkaj <ienzkajed@icloud.com>
2023-12-08 20:34:46 +0100

    feat: fixes

--- src/cat-api.js
+++ src/cat-api.js
@@ -6,3 +6,9 @@ axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
 export function fetchBreeds() {
   return axios.get(`/breeds`).then(({ data }) => data);
 }
+
+export function fetchCatByBreed(breedId) {
+  return axios
+    .get(`/images/search?breed_ids=${breedId}`)
+    .then(({ data }) => data);
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { fetchBreeds } from './cat-api';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 axios.defaults.headers.common['x-api-key'] =
   'live_LHRQcmoVBhX3YZHvL7nrtF4rN9Y05A8EDvVpjKcJJBzOof6luUKwGvnJthT7liq3';
@@ -11,18 +11,13 @@ Notiflix.Notify.init({
   position: 'right-top',
 });
 
-function fetchCatByBreed(breedId) {
-  return axios
-    .get(`/images/search?breed_ids=${breedId}`)
-    .then(({ data }) => data);
-}
-
 /** @type {HTMLSelectElement | null} */
 const breeds = document.querySelector('.breed-select');
 
 const effect = document.querySelector('div.cat-info');
 
 fetchBreeds().then(data => {
+  document.querySelector(`p.loader`).classList.add('hidden');
   const html = data.map(
     breed => `<option value = "${breed.id}"> ${breed.name} </option>`
   );
@@ -32,23 +27,33 @@ fetchBreeds().then(data => {
 
 breeds.addEventListener('change', ev => {
   document.querySelector(`p.loader`).classList.remove('hidden');
+  effect.classList.add('hidden');
   const breedId = ev.target.value;
-  fetchCatByBreed(breedId).then(cats => {
-    document.querySelector(`p.loader`).classList.add('hidden');
-    if (cats.length == 0) {
+
+  fetchCatByBreed(breedId)
+    .then(cats => {
+      document.querySelector(`p.loader`).classList.add('hidden');
+      if (cats.length == 0) {
+        Notiflix.Notify.failure(
+          'Oops! Something went wrong! Try reloading the page!'
+        );
+
+        effect.innerHTML = ``;
+      } else {
+        const array = cats.map(
+          cat =>
+            ` <h2 >${cat.breeds[0].name}</h2><img width="50%vw" height="400px" src="${cat.url}" /> <div><p>${cat.breeds[0].description}</p>
+        <p>TEMPERAMENT: ${cat.breeds[0].temperament}</p></div>`
+        );
+
+        effect.innerHTML = array.join('');
+        effect.classList.remove('hidden');
+      }
+    })
+    .catch(error => {
+      document.querySelector(`p.loader`).classList.add('hidden');
       Notiflix.Notify.failure(
         'Oops! Something went wrong! Try reloading the page!'
       );
-
-      effect.innerHTML = ``;
-    } else {
-      const array = cats.map(
-        cat =>
-          ` <h2 >${cat.breeds[0].name}</h2><img width="50%vw" height="400px" src="${cat.url}" /> <div><p>${cat.breeds[0].description}</p>
-        <p>TEMPERAMENT: ${cat.breeds[0].temperament}</p></div>`
-      );
-
-      effect.innerHTML = array.join('');
-    }
-  });
+    });
 });
